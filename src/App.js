3d750bc84bde71579088020e17c09c8967206e4a
mnwachukwu042 <mnwachukwu042@gmail.com>
2022-10-03 23:58:33 +0100

    use of material UI

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Greet from "./components/Greet";
 import FunctionClick from "./components/FunctionClick";
 import NameList from "./components/NameList";
 import PersonList from "./components/PersonList";
+import Page2 from "./components/Page2";
 function App() {
   return (
     <div className="App">
+      <Page2 />
       <FunctionClick />
       <PersonList />
       <Greet name="Israel" profession="CEO">
